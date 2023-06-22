ee41fddf806270a089152488294e7fc18e768c7a
himanushi <himanushi@users.noreply.github.com>
2023-06-23 07:03:24 +0900

    ios

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useBrowserBack } from "~/hooks";
 export default () => {
   useBrowserBack();
   return (
-    <App theme="md" name="app" routes={routes}>
+    <App theme="ios" name="app" routes={routes}>
       <View
         browserHistory
         browserHistorySeparator=""
