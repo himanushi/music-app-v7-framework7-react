8e53a7681f7b82f6f46fb8d812bdadedb6224b38
himanushi <himanushi@users.noreply.github.com>
2023-06-23 06:46:39 +0900

    add Albums page

---
+++ src/pages/Albums.tsx
@@ -0,0 +1,10 @@
+import { Link, Navbar, Page } from "framework7-react";
+
+export const Albums = () => {
+  return (
+    <Page name="albums">
+      <Navbar>aa</Navbar>
+      <Link href="/albums/1">About Page</Link>albums
+    </Page>
+  );
+};
