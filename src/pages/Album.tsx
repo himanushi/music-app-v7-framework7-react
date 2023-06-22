8ac01273a2573eb0a26b63277966ae4edf424652
himanushi <himanushi@users.noreply.github.com>
2023-06-23 08:12:03 +0900

    add nav bar

--- src/pages/Album.tsx
+++ src/pages/Album.tsx
@@ -1,8 +1,9 @@
-import { Page } from "framework7-react";
+import { Navbar, Page } from "framework7-react";
 
 export const Album = () => {
   return (
     <Page name="album">
+      <Navbar title="Album" />
       album<input></input>
     </Page>
   );
--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -3,7 +3,7 @@ import { Link, Navbar, Page } from "framework7-react";
 export const Albums = () => {
   return (
     <Page name="albums">
-      <Navbar>aa</Navbar>
+      <Navbar title="Albums" />
       <Link href="/albums/1">About Page</Link>albums
     </Page>
   );
