206a0e3b74a55a372f56e6ceb9000f7e91a3c5a5
himanushi <himanushi@users.noreply.github.com>
2023-06-24 07:37:03 +0900

    test virtual list

--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -1,10 +1,91 @@
-import { Link, Navbar, Page } from "framework7-react";
+import React, { useState } from "react";
+import {
+  Navbar,
+  Page,
+  List,
+  ListItem,
+  Subnavbar,
+  Searchbar,
+  Block,
+  theme,
+} from "framework7-react";
 
 export const Albums = () => {
+  const items: any[] = [];
+  for (let i = 1; i <= 10000; i += 1) {
+    items.push({
+      title: `Item ${i}`,
+      subtitle: `Subtitle ${i}`,
+    });
+  }
+  const [vlData, setVlData] = useState({
+    items: [],
+    topPosition: 0,
+  });
+
+  const searchAll = (query: any, searchItems: any) => {
+    const found = [];
+    for (let i = 0; i < searchItems.length; i += 1) {
+      if (
+        searchItems[i].title.toLowerCase().indexOf(query.toLowerCase()) >= 0 ||
+        query.trim() === ""
+      )
+        found.push(i);
+    }
+    return found; // return array with mathced indexes
+  };
+  const renderExternal = (vl: any, newData: any) => {
+    setVlData({ ...newData });
+  };
   return (
-    <Page name="albums">
-      <Navbar title="Albums" />
-      <Link href="/albums/1">About Page</Link>albums
+    <Page>
+      <Navbar title="Virtual List"></Navbar>
+      <Block>
+        <p>
+          Virtual List allows to render lists with huge amount of elements
+          without loss of performance. And it is fully compatible with all
+          Framework7 list components such as Search Bar, Infinite Scroll, Pull
+          To Refresh, Swipeouts (swipe-to-delete) and Sortable.
+        </p>
+        <p>Here is the example of virtual list with 10 000 items:</p>
+      </Block>
+      <List
+        strong
+        outlineIos
+        insetMd
+        dividersIos
+        className="searchbar-not-found"
+      >
+        <ListItem title="Nothing found" />
+      </List>
+      <List
+        strong
+        outlineIos
+        insetMd
+        dividersIos
+        className="searchbar-found"
+        virtualList
+        virtualListParams={{
+          items,
+          searchAll,
+          renderExternal,
+          height: theme.ios ? 63 : theme.md ? 73 : 77,
+        }}
+      >
+        <ul>
+          {vlData.items.map((item: any, index) => (
+            <ListItem
+              key={index}
+              mediaItem
+              link={`/albums/${index}`}
+              title={item.title}
+              subtitle={item.subtitle}
+              style={{ top: `${vlData.topPosition}px` }}
+              virtualListIndex={items.indexOf(item)}
+            />
+          ))}
+        </ul>
+      </List>
     </Page>
   );
 };
